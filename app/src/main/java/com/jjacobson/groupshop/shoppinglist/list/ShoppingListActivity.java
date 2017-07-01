8c96e082df796e4b70f8d687eedfb8af7bce1f85
Jeremiah Jacobson <j.jacobson966@gmail.com>
2017-07-01 02:47:29 -0800

    Add navbar drawer to main list

--- app/src/main/java/com/jjacobson/groupshop/shoppinglist/list/ShoppingListActivity.java
+++ app/src/main/java/com/jjacobson/groupshop/shoppinglist/list/ShoppingListActivity.java
@@ -18,7 +18,7 @@ public class ShoppingListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_add_list);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/jjacobson/groupshop/shoppinglist/menu/MenuListActivity.java
+++ app/src/main/java/com/jjacobson/groupshop/shoppinglist/menu/MenuListActivity.java
@@ -3,6 +3,8 @@ package com.jjacobson.groupshop.shoppinglist.menu;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -18,14 +20,11 @@ public class MenuListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
 }
