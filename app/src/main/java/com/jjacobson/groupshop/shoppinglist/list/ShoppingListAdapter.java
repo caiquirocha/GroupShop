55f82598eb79084ad3d3503a1edf96e216b08108
Jeremiah Jacobson <j.jacobson966@gmail.com>
2017-07-28 00:39:21 -0800

    Update Icon

--- app/src/main/java/com/jjacobson/groupshop/shoppinglist/list/ShoppingListAdapter.java
+++ app/src/main/java/com/jjacobson/groupshop/shoppinglist/list/ShoppingListAdapter.java
@@ -1,9 +1,13 @@
 package com.jjacobson.groupshop.shoppinglist.list;
 
 import android.support.annotation.LayoutRes;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.Query;
+import com.jjacobson.groupshop.R;
 import com.jjacobson.groupshop.shoppinglist.item.Item;
 
 /**
@@ -15,6 +19,14 @@ public class ShoppingListAdapter extends FirebaseRecyclerAdapter<Item, ShoppingL
     public ShoppingListAdapter(Class<Item> modelClass, @LayoutRes int modelLayout, Class<ShoppingListHolder> viewHolderClass, Query query) {
         super(modelClass, modelLayout, viewHolderClass, query);
     }
+
+    @Override
+    public ShoppingListHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.row_shopping_list, parent, false);
+        return new ShoppingListHolder(view);
+    }
+
 
     @Override
     protected void populateViewHolder(ShoppingListHolder viewHolder, Item model, int position) {
