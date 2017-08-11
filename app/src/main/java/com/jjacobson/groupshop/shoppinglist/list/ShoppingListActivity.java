4aa5a387c3ab3c761e9f02369103273b93c44346
Jeremiah Jacobson <j.jacobson966@gmail.com>
2017-08-11 00:07:59 -0800

    Add option to remove purchased items. Add icon for sharing list.

--- app/src/main/java/com/jjacobson/groupshop/shoppinglist/list/ShoppingListActivity.java
+++ app/src/main/java/com/jjacobson/groupshop/shoppinglist/list/ShoppingListActivity.java
@@ -2,7 +2,9 @@ package com.jjacobson.groupshop.shoppinglist.list;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,9 +14,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.jjacobson.groupshop.R;
 import com.jjacobson.groupshop.shoppinglist.item.Item;
 import com.jjacobson.groupshop.shoppinglist.item.ItemPropertyDialog;
@@ -58,7 +63,8 @@ public class ShoppingListActivity extends AppCompatActivity {
             finish();
             return true;
         }
-        if (id == R.id.action_list_settings) {
+        if (id == R.id.action_list_delete_purchased) {
+            deletePurchased();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -91,6 +97,31 @@ public class ShoppingListActivity extends AppCompatActivity {
         items.addItemDecoration(divider);
         items.setLayoutManager(new LinearLayoutManager(this));
         items.setAdapter(adapter);
+    }
+
+    /**
+     * Delete all purchased items
+     */
+    private void deletePurchased() {
+        Query query = database.orderByChild("purchased").equalTo(true);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                long count = dataSnapshot.getChildrenCount();
+                String text = getResources().getString(R.string.snackbar_delete_purchased, count);
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    snapshot.getRef().removeValue();
+                }
+                CoordinatorLayout layout = (CoordinatorLayout) findViewById(R.id.list_coordinator_layout);
+                Snackbar.make(layout, text, Snackbar.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     public List getList() {
