dff48280ebcbc892cd0fd26a050ae348685a30e5
Jeremiah Jacobson <j.jacobson966@gmail.com>
2017-09-04 03:02:53 -0800

    Add some logic to complete profile edit button

--- app/src/main/java/com/jjacobson/groupshop/sharing/profile/CompleteButtonListener.java
+++ app/src/main/java/com/jjacobson/groupshop/sharing/profile/CompleteButtonListener.java
@@ -1,6 +1,11 @@
 package com.jjacobson.groupshop.sharing.profile;
 
+import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
+
+import com.jjacobson.groupshop.R;
+import com.jjacobson.groupshop.sharing.users.UserSaveActivity;
 
 /**
  * Created by Jeremiah on 8/22/2017.
@@ -10,12 +15,25 @@ class CompleteButtonListener implements View.OnClickListener {
 
     private EditProfileActivity activity;
 
+    private EditText nameText;
+    private EditText emailText;
+
     public CompleteButtonListener(EditProfileActivity activity) {
         this.activity = activity;
+        this.nameText = (EditText) activity.findViewById(R.id.profile_name_text);
+        this.emailText = (EditText) activity.findViewById(R.id.profile_email_text);
     }
 
     @Override
     public void onClick(View view) {
+        String name = nameText.getText().toString();
+        String email = emailText.getText().toString();
+        activity.getUserProfile().setName(name);
+        activity.getUserProfile().setEmail(email);
 
+        Intent intent = new Intent(activity, UserSaveActivity.class);
+        intent.putExtra("user_extra", activity.getUserProfile());
+        activity.startActivity(intent);
+        activity.finish();
     }
 }
